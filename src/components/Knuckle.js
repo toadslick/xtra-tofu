e5d0d1b9fe4bb78ba60fb4d647211e4075195e17
James Childress <james.childress@zapier.com>
2019-12-22 11:40:36 -0800

    play sound

--- src/components/Knuckle.js
+++ src/components/Knuckle.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import load from 'audio-loader';
+import play from 'audio-play';
 
 import IMAGE_URL from '../assets/knux.png';
 
 const IMAGE_HEIGHT = 239;
 
 const Knuckle = ({ offset, width, url, active }) => {
+  const [buffer, setBuffer] = useState(null);
+
+  useEffect(() => {
+    load(url).then(setBuffer);
+  }, [url]);
+
+  useEffect(() => {
+    if (active && buffer) {
+      play(buffer, {
+        start: 0,
+        autoplay: true,
+      });
+    }
+  }, [active, buffer]);
+
   return (
     <div
       style={{
