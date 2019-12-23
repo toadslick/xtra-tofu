8f98dd334658c87f69a0e85da56dcaecfe3ca5be
James Childress <james.childress@zapier.com>
2019-12-22 16:19:05 -0800

    Updates

---
+++ precache-manifest.206eb4e91cf8b9d1876ecd964703daeb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b89fdee55fe016726a69d90b5cead7",
+    "url": "/xtra-tofu/index.html"
+  },
+  {
+    "revision": "51e6bf43fc9a4d9f1de8",
+    "url": "/xtra-tofu/static/css/main.3f09a245.chunk.css"
+  },
+  {
+    "revision": "b559302375ecb98fad40",
+    "url": "/xtra-tofu/static/js/2.a66db627.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/xtra-tofu/static/js/2.a66db627.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51e6bf43fc9a4d9f1de8",
+    "url": "/xtra-tofu/static/js/main.9af78e47.chunk.js"
+  },
+  {
+    "revision": "bae84edd5497cb64e686",
+    "url": "/xtra-tofu/static/js/runtime-main.075d6af3.js"
+  },
+  {
+    "revision": "5e0f53e9420908bf1e6b97637c0d74db",
+    "url": "/xtra-tofu/static/media/chord-c.5e0f53e9.mp3"
+  },
+  {
+    "revision": "88795f441a1de47e0b2c3e3f62c7090f",
+    "url": "/xtra-tofu/static/media/chord-d.88795f44.mp3"
+  },
+  {
+    "revision": "7a3a26eeeb2bc68d8c2e6631e80c2178",
+    "url": "/xtra-tofu/static/media/chord-f.7a3a26ee.mp3"
+  },
+  {
+    "revision": "8fc3d1036d38dd5a5407b1a460a5b751",
+    "url": "/xtra-tofu/static/media/chord-g.8fc3d103.mp3"
+  },
+  {
+    "revision": "1cbc7987a393e585b3b95e067fa30854",
+    "url": "/xtra-tofu/static/media/hi-hat.1cbc7987.mp3"
+  },
+  {
+    "revision": "cfecbceb6e4dc6aaa84fd01d9373de43",
+    "url": "/xtra-tofu/static/media/kick.cfecbceb.mp3"
+  },
+  {
+    "revision": "1d8677b8cd6688e9b2e7b122560c633d",
+    "url": "/xtra-tofu/static/media/knux.1d8677b8.png"
+  },
+  {
+    "revision": "af17665ed0e53a83f47f538f137263a3",
+    "url": "/xtra-tofu/static/media/reed.af17665e.mp3"
+  },
+  {
+    "revision": "5e19314e869bbd5b813fa435ec8e7516",
+    "url": "/xtra-tofu/static/media/snare.5e19314e.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/xtra-tofu/precache-manifest.206eb4e91cf8b9d1876ecd964703daeb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xtra-tofu/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
